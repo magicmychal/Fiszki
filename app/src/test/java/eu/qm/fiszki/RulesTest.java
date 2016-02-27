c707e5da21ba20c548e3b7f3912a83ecb34c6bfa
Michal P <michalpawlicki@outlook.com>
2016-02-27 16:44:52 +0100

    fail test expected

--- app/src/test/java/eu/qm/fiszki/RulesTest.java
+++ app/src/test/java/eu/qm/fiszki/RulesTest.java
@@ -20,6 +20,6 @@ public class RulesTest {
     @Test
     public void testCheck() throws Exception {
         assertTrue(tRules.Check(new String("chuj"),new String("chuj")));
-        assertFalse(tRules.Check(new String("chuj"),new String("jaro")));
+        assertFalse(tRules.Check(new String("chuj"),new String("chuj")));
     }
 }
